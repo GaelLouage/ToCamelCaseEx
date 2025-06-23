9aca34f89257771b3c4f5d88e4f108306fb1b974
Gaël Louage <gaellouage@hotmail.com>
2025-06-23 11:43:00 +0200

    toCamelCase exercise

---
+++ ToCamelCaseEx/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(ToCamelCaseLinq("the_stealth_warrior"));
+        Console.WriteLine(ToCamelCaseLinq("The-Stealth-Warrior"));
+    }
+
+    public static string ToCamelCase(string str)
+    {
+        var sb = new StringBuilder();
+        str = str.Replace("-", "_");
+        var splittedString = str.Split('_');
+        for(int i = 0; i < splittedString.Length; i++)
+        {
+            if(i == 0)
+            {
+                sb.Append($"{splittedString[i]}");
+            }
+            else
+            {
+                var firstLetter = splittedString[i].Substring(0, 1).ToUpper();
+                var lastParthOfString = splittedString[i].Substring(1);
+                sb.Append($"{firstLetter}{lastParthOfString}");
+            }
+        }
+        return sb.ToString();
+    }
+
+    public static string ToCamelCaseLinq(string str) =>
+        string.Concat((str
+            .Split('-', '_')
+            .Select((x,i) => i == 0 ? x : $"{char.ToUpper(x[0])}{x.Substring(1)}")));
+    
+}
